6395026e8e3fe9f6a146c4772a9bd68bfaa1b97a
amsakanna <amsakanna@gmail.com>
2021-06-03 19:20:53 +0530

    Add better logging for shell scripts
    1. Add logger.sh.
    2. Add error_thrower.sh.
    3. Format all scripts to call logger and error_thrower.
    
    Fix #13

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -10,24 +10,59 @@
 #			- Formats Code
 #			- Runs dart analyze
 #
-#		Arguments:
-#			<none>
+#		Arguments: <none>
 #
 #-----------------------------------------------------------------------------
 
-# Abort if there is an error.
+# SETUP
 set -e
+scriptDirectory="$(dirname "$0")"
+scriptName="$(basename "$0")"
+source "$scriptDirectory/logger.sh" $scriptName
+errorList=(
+	"DART_PUB_GET_ERROR"
+	"DART_FORMAT_ERROR"
+	"DART_ANALYZE_ERROR"
+)
+source "$scriptDirectory/error_thrower.sh" $scriptName $errorList
 
-# Get dependencies
+#
+# Getting Dependencies
+#
+logStep "Getting Dependencies"
+echo "Running dart pub get"
 dart pub get
+exitCode=$?
+if [[ exitCode -ne 0 ]]; then
+	throwAndExit "DART_PUB_GET_ERROR" "Error while running dart pub get"
+fi
 
-# Format code
-dart format --set-exit-if-changed .
+#
+# Formatting Code
+#
+logStep "Formatting Code"
+echo "Running dart format ."
+dart format .
+exitCode=$?
+if [[ exitCode -ne 0 ]]; then
+	throwAndExit "DART_FORMAT_ERROR" "Error while running dart format"
+fi
 
-# Run dart analyze
+#
+# Analyzing Code
+#
+logStep "Analyzing Code"
+echo "Running dart analyze --fatal-infos --fatal-warnings ."
 dart analyze --fatal-infos --fatal-warnings .
+exitCode=$?
+if [[ exitCode -ne 0 ]]; then
+	throwAndExit "DART_ANALYZE_ERROR" "Error while running dart analyze"
+fi
 
-# End
+#
+# END
+#
+logEnd
 exit 0
 
 #-----------------------------------------------------------------------------
--- scripts/test.sh
+++ scripts/test.sh
@@ -8,18 +8,37 @@
 #		What it does:
 #			- Runs tests and collects coverage
 #
-#		Arguments:
-#			<none>
+#		Arguments: <none>
 #
 #-----------------------------------------------------------------------------
 
-# Abort if there is an error.
+#
+# SETUP
+#
 set -e
+scriptDirectory="$(dirname "$0")"
+scriptName="$(basename "$0")"
+source "$scriptDirectory/logger.sh" $scriptName
+errorList=(
+	"FLUTTER_TEST_ERROR"
+)
+source "$scriptDirectory/error_thrower.sh" $scriptName $errorList
 
-# Run tests and collect coverage
+#
+# Running Tests and Collecting Coverage
+#
+logStep "Running Tests and Collecting Coverage"
+echo "Running flutter test --no-pub --coverage"
 flutter test --no-pub --coverage
+exitCode=$?
+if [[ exitCode -ne 0 ]]; then
+	throwAndExit "FLUTTER_TEST_ERROR" "Error while running flutter test"
+fi
 
-# End
+#
+# END
+#
+logEnd
 exit 0
 
 #-----------------------------------------------------------------------------
