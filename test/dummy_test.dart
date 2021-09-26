94b7d51b4271aec1b227d061c6c0c358260c76f4
amsakanna <amsakanna@gmail.com>
2021-09-26 19:37:00 +0530

    Feature: zam_test sample test in dummy_test.dart Fixes #35

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,7 +1,18 @@
 import 'package:zam_test/zam_test.dart';
 
 void main() {
-  test('Dummy Test', () {
-    expect(1, 1);
-  });
+  DummyTest().execute();
+}
+
+class DummyTest extends Test<int, bool> {
+  @override
+  bool run(int input) {
+    return input.isOdd;
+  }
+
+  @override
+  final cases = [
+    BooleanTestCase.truthy(when: 'Odd number', input: 57),
+    BooleanTestCase.falsy(when: 'Even number', input: 388),
+  ];
 }
